f71c8027903ee1aeb8a4c20144b3ff194ef6ef96
snowflakeljenkins <130600518+snowflakeljenkins@users.noreply.github.com>
2023-04-13 07:55:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,5 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.header('Fruityvice Fruit Advice!')
 streamlit.text(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
